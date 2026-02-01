1028a7ef794fe3ead46e2cd752dbf2a417b7404c
Konstantin Popov <costachille@Konstantin.local>
2026-02-01 15:18:53 +0800

    first commit

---
+++ ui-kit-dz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ui-kit-dz
+//
+//  Created by Konstantin Popov on 01.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
